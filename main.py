9ab9891f617cd137cc38fb1aeef19789a9561bd4
remi <remi@remialkemade.nl>
2017-11-26 12:03:19 +0100

    Added demo texts

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from botfather_commander import BotfatherCommander
 import time
 
 # Hackathon-style tokens and IDs!
-SLACK_BOT_TOKEN = 'xoxb-277615982901-QpYOuR6ck9co1gled5dIjpIm'
+SLACK_BOT_TOKEN = 'xoxb-277615982901-ZNwZTlYBL3RPwgO3e4gdg2fR'
 BOT_ID = "U85J3UWSH"
 
 READ_WEBSOCKET_DELAY = 1  # 1 second delay between reading from firehose
