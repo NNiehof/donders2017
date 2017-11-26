93439dbcfdea702bb189c41d6519b15a743cf17b
Yasen <ydinkov@slim-orange>
2017-11-26 10:56:09 +0100

    success

--- botfather_REMOTE_12841.py
+++ botfather_REMOTE_12841.py
@@ -12,9 +12,9 @@ class BotFather:
         self.slackClient = SlackClient(slack_bot_token)
 
         self.achievements = {}        
-        self.achievements["^(?=.*\\bassassino\\b)(?=.*\\bgladiatore\\b).*$"] = "assassino"
-        self.achievements["^(?=.*\\bmotivo\\b)(?=.*\\bvendetta\\b).*$"] = "assassino"
-        self.achievements["^(?=.*\\barma\\b)(?=.*\\bcoltello\\b).*$"] = "arma"
+        self.achievements['^(?=.*\\bassassino\\b)(?=.*\\bgladiatore\\b).*$'] = 'murderer'
+        self.achievements['^(?=.*\\bmotivo\\b)(?=.*\\bvendetta\\b).*$'] = 'motive'
+        self.achievements['^(?=.*\\barma\\b)(?=.*\\bcoltello\\b).*$'] = 'weapon'
         
         self.usernames = self.load_users()
         self.atBot = "<@" + bot_id + ">"
@@ -24,12 +24,12 @@ class BotFather:
 
 
         # Init language check
-        self.language = language_check.LanguageTool("it-IT")
+        self.language = language_check.LanguageTool('it-IT')
         self.n_learned = 0
 
         # Init AIML
         self.kernel = aiml.Kernel()
-        self.kernel.learn("botfather.xml")
+        self.kernel.learn('botfather.xml')
 
     def post(self, text, channel):
         result = self.slackClient.api_call("chat.postMessage", channel=channel, text=text, as_user=True)
@@ -38,41 +38,41 @@ class BotFather:
 
     def get_channel_name(self, channel_id):
         info = self.slackClient.api_call("groups.info", channel=channel_id)
-        if info and "group" in info:
-            return info["group"]["name"]
+        if info and 'group' in info:
+            return info['group']['name']
         
-        return "Untitled Document 1"
+        return 'Untitled Document 1'
 
     def parse_slack_output(self, slack_rtm_output):
         output_list = slack_rtm_output
         if output_list and len(output_list) > 0:
             for output in output_list:
                 # act upon messages that are not its own
-                if output and "text" in output and "user" in output and output["user"] != self.botID:
+                if output and 'text' in output and 'user' in output and output['user'] != self.botID:
                     # AIML
-                    response = self.kernel.respond(output["text"])
+                    response = self.kernel.respond(output['text'])
                     if response:
-                        self.post(response, output["channel"])
-                    user, self.n_learned = self.learning_progress(output["user"], output["text"],output["channel"])
+                        self.post(response, output['channel'])
+                    user, self.n_learned = self.learning_progress(output['user'], output['text'])
                     # Language check
-                    correction = self.check_language(output["text"])
+                    correction = self.check_language(output['text'])
                     if correction is not None:
-                        self.post(correction, output["channel"])
+                        self.post(correction, output['channel'])
 
                     # Find myname-othername channels
-                    channel = self.get_channel_name(output["channel"])
+                    channel = self.get_channel_name(output['channel'])
                     match = re.search(r"([A-Za-z0-9]+)-([A-Za-z0-9]+)", channel)
                     print(match)
                     if match:
-                        self.direct_message(output["text"], match.group(1), match.group(2), output["user"])
+                        self.direct_message(output['text'], match.group(1), match.group(2), output['user'])
 
-    def learning_progress(self, user, text, channel):
-        """Add user text input to that user"s list of learned words,
+    def learning_progress(self, user, text):
+        """Add user text input to that user's list of learned words,
         if the words are unique and correct
         """
         if self.wordFilter.filter_text(text) is None:
-            self.check_italian(user,text,channel)
-            for word in text:
+            self.check_italian(user,text)
+            for word in text.split():
                 if word not in self.learned_words[user]:
                     self.learned_words[user].append(word)
         n_learned = len(self.learned_words[user])
@@ -85,16 +85,14 @@ class BotFather:
             correction = language_check.correct(txt, matches)
             for match in matches:
                 if len(match.replacements) > 0:
-                    return "Did you mean "" + correction + ""?"
+                    return "Did you mean '" + correction + "'?"
             return "TARTA DI MELE!"
         return None
 
-
-
     def direct_message(self, text, from_user, to_user, from_user_id):
         filtered = self.wordFilter.filter_text(text)
-        home_channel = from_user + "-" + to_user
-        away_channel = to_user + "-" + from_user
+        home_channel = from_user + '-' + to_user
+        away_channel = to_user + '-' + from_user
         if filtered is None:
             # if all words are allowed, pass the message along to other user
             if from_user_id in self.usernames:
@@ -106,7 +104,7 @@ class BotFather:
                 self.n_learned), home_channel)
         else:
             # if disallowed words present, notify the sender
-            text = "Cannot use the word "" + filtered + ""."
+            text = "Cannot use the word '" + filtered + "'."
             self.post(text, home_channel)
 
     def connect(self):
@@ -120,26 +118,23 @@ class BotFather:
         input = self.slackClient.rtm_read()
         self.parse_slack_output(input)
 
-    def check_italian(self, user, text,channel):
-        for key in self.achievements:            
+    def check_italian(self, user, text):
+        print(user + " " + text)
+        for key in self.achievements:
+            print("\t key:" + key)   
             if re.match(key,text): 
-                self.usernames[user][self.achievements[key]] = 1
-                print(self.usernames[user])
-                self.score(user,self.achievements[key],channel)
-
-    def score(self,user,clue, channel):
-        self.post("Complimenti! hai trovato il " + clue + "!", channel)
-
+                json.loads(str(self.usernames[user]))["text"] = 1
+                print(json.dumps(self.usernames[user]))
 
     def load_users(self):
         json_data = json.dumps(self.slackClient.api_call("users.list"))
         json_obj = json.loads(json_data)
-        usernames = {"":""}
+        usernames = {'':''}
         for _item in json_obj["members"]:
             _item["murderer"]=0
             _item["motive"]=0
             _item["weapon"]=0
-            if not _item["is_bot"] and _item["id"] != "USLACKBOT":
+            if not _item["is_bot"] and _item["id"] != 'USLACKBOT':
                 usernames[_item["id"]] = json.loads(str(json.dumps(_item)))
             #print("\n"+str(_item))
         return usernames
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from botfather_commander import BotfatherCommander
 import time
 
 # Hackathon-style tokens and IDs!
-SLACK_BOT_TOKEN = 'xoxb-277615982901-zN8QxHQf53TEuqrGkxab7LFO'
+SLACK_BOT_TOKEN = 'xoxb-277615982901-QpYOuR6ck9co1gled5dIjpIm'
 BOT_ID = "U85J3UWSH"
 
 READ_WEBSOCKET_DELAY = 1  # 1 second delay between reading from firehose
