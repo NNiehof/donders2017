1fe50dbda58e740313061c265424fe29b6c6db7b
kachergis <george.kachergis@gmail.com>
2017-11-26 01:01:32 +0100

    I'm happy with botfather555_EN, and will finish translating (_IT) early AM, and integrate with other...

--- botfather555_IT.py
+++ botfather555_IT.py
@@ -16,22 +16,22 @@ START_GAME = "start"
 
 GAME_STARTED = False
 
-intro = ["Welcome to the Starlight Casino bar! Tonight we have the best Elvis impersonator in Las Vegas. Please enjoy the show.",
-    "As the show starts--BANG!--a loud gun shot is heard. The curtain comes up, and Elvis falls over. The casino's bar doors slam shut.]",
-    "Everyone here is now a suspect to the murder of the Elvis impersonator. Please talk to everyone, and help me find out who the murderer is.",
-    "After 15 minutes, I will ask who you think is the murderer. If you want help, please type @Botfather help language",
-    "Try to find out 1) WHO is the murderer, 2) WHY did she/he commit the murder, and 3) HOW did they commit the murder."]
+intro = ["Benvenuti allo Starlight Casinò! Stasera abbiamo con noi il miglior sosia di Elvis di Las Vegas. Godetevi lo spettacolo.",
+    "Appena lo spettacolo inizia, si sente un forte sparo. Il sipario di alza, ed Elvis cade. La porta del casinò sbatte.",
+    "Tutti i presenti sono ora sospettati dell'omicidio del sosia di Elvis. Parlate tra di voi, e aiutatemi a capire chi è l'assassino.",
+    "Dopo 15 minuti, vi chiederò chi pensate sia l'assassino. Se hai bisogno di aiuto, scrivi @botfather help",
+    "Cerca di capire 1) CHI è l'assassino/a, 2) PERCHE' ha ucciso e 3) COME ha commesso l'omicidio."]
 
-lang_tips = ["Try asking who has been to the casino before: 'Have you been to the casino before?'",
-    "Try to find out who is married: 'Are you married?'",
-    "Find out who has a gun: 'Do you have guns?'"]
+lang_tips = ["Try asking who has been to the casino before: 'Sei stato al casinò prima d'ora?'",
+    "Try to find out who is married: 'Sei sposato?'",
+    "Find out who has a gun: 'Hai pistole?'"]
 
-tips = ["Very few of us are what we seem. -Agatha Christie",
-    "When you have eliminated the impossible, whatever remains, however improbable, must be the truth. -Sherlock Holmes",
-    "Good advice is always certain to be ignored, but that's no reason not to give it. -Agatha Christie",
-    "Just when I thought I was out, they pull me back in.",
-    "I'm going to make him an offer he can't refuse.",
-    "Revenge is a dish best served cold."]
+tips = ["Solo pochi tra noi sono quello che sembrano. -Agatha Christie",
+    "Una volta che hai eliminato l'impossibile, quel che rimane, per quanto improbabile, dev'essere la verità. -Sherlock Holmes",
+    "Un buon consiglio sarà quasi certamente ignorato, ma non c'è motivo di non darlo. -Agatha Christie",
+    "Proprio quando pensavo di esserne fuori, mi tirano di nuovo in mezzo.",
+    "Gli farò una proposta che non può rifiutare.",
+    "La vendetta è un piatto da servire freddo."]
 
 lang_tips_ind = 0
 tips_ind = 0
@@ -45,15 +45,15 @@ private_msg = {
         "You have never been to the casino before.",
         "Clue: You have been following Elvis for a story and you know that he was in a relationship with someone who was married."],
     "tutor": ["tutors gotta tute"],
-    "gunnut": ["You own several guns, as a true American.",
-        "You and your wife are big Elvis fans.",
-        "But you saw this this Elvis impersonator before, and think he is terrible.",
-        "You have been to several of his other shows and heckle him on and off stage.",
-        "You are NOT the murderer.",
-        "Clue: You lost your gun at the casino bar last week."],
-    "escort": ["You are an escort hired by many of the Starlight Casino's most famous guests.", 
-        "You are proud to be high-class, and would never mix with street prostitutes.",
-        "Clue: Elvis once brought the Escort to a wedding, and she tried to fool around with him afterwards but he was not interested."],
+    "gunnut": ["Possiedi diverse pistole.",
+        "Tu e tua moglie siete grandi fan di Elvis.",
+        "Ma tu hai visto questo sosia di Elvis e pensi che sia pessimo.",
+        "Hai assistito ad altri suoi spettacoli e hai manifestato l'hai fischiato mentre era sul palco e a spettacolo finito.",
+        "NON sei l'assassino.",
+        "Indizio: Hai perso la tua pistola al casinò la scorsa settimana."],
+    "escort": ["Intrattieni alcuni tra i più stimati ospiti del Starlight Casino.", 
+        "Sei orgogliosa di essere una di alto bordo, e non ti mischieresti mai alle battone.",
+        "Indizio: una volta Elvis ha portato la Escort a un matrimonio, e lei dopo ci aveva provato con lui, ma lui non era interessato."],
     "gladiator" : ["You are a recently-fired gladiator, and used to be best friends with Elvis.", 
         "You were previously employed by the casino to entertain the gamblers.", 
         "Elvis revealed to the owner that you were drinking on the job.",
